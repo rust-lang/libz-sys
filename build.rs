1a66c203b3676989e322f74b668a2e9e94685413
Alex Crichton <alex@alexcrichton.com>
2015-07-22 12:29:02 -0700

    Use gcc's support to find nmake

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 extern crate pkg_config;
+extern crate gcc;
 
 use std::env;
 use std::ffi::OsString;
@@ -22,13 +23,13 @@ fn main() {
     // one of those sole platforms that doesn't!
     let target = env::var("TARGET").unwrap();
     if target.contains("msvc") {
-        build_msvc_zlib();
+        build_msvc_zlib(&target);
     } else {
         println!("cargo:rustc-link-lib=z");
     }
 }
 
-fn build_msvc_zlib() {
+fn build_msvc_zlib(target: &str) {
     let src = t!(env::current_dir()).join("src/zlib-1.2.8");
     let dst = PathBuf::from(env::var_os("OUT_DIR").unwrap());
 
@@ -37,12 +38,14 @@ fn build_msvc_zlib() {
 
     let mut top = OsString::from("TOP=");
     top.push(&src);
-    run(Command::new("nmake")
-                .current_dir(dst.join("lib"))
-                .arg("/f")
-                .arg(src.join("win32/Makefile.msc"))
-                .arg(top)
-                .arg("zlib.lib"));
+    let nmake = gcc::windows_registry::find(target, "nmake.exe");
+    let mut nmake = nmake.unwrap_or(Command::new("nmake.exe"));
+    run(nmake.current_dir(dst.join("lib"))
+             .arg("/nologo")
+             .arg("/f")
+             .arg(src.join("win32/Makefile.msc"))
+             .arg(top)
+             .arg("zlib.lib"));
 
     for file in t!(fs::read_dir(&src)) {
         let file = t!(file).path();
