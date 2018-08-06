d52034109e6eeb7e1fb72cadcc364f7d961b5882
Alex Crichton <alex@alexcrichton.com>
2018-08-06 08:01:22 -0700

    Never run pkg-config on MSVC
    "it works" seems like it's a lie

--- build.rs
+++ build.rs
@@ -32,6 +32,7 @@ fn main() {
     // (Homebrew, Ports, etc.)
     let want_static = env::var("LIBZ_SYS_STATIC").unwrap_or(String::new()) == "1";
     if !want_static &&
+       !target.contains("msvc") && // pkg-config just never works here
        !(host_and_target_contain("apple") ||
          host_and_target_contain("freebsd") ||
          host_and_target_contain("dragonfly")) &&
