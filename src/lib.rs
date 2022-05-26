7357fa9d37c0a45f4492a8d4f5c18e6b87fd98d0
Josh Triplett <josh@joshtriplett.org>
2022-05-25 22:56:02 -0700

    Run `cargo fmt`
    This leads to a bit of churn, but will hopefully make further changes
    easier.

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,10 @@ pub type voidpf = *mut c_void;
 pub enum gzFile_s {}
 pub enum internal_state {}
 
-#[cfg(all(feature = "libc", not(all(target_family = "wasm", target_os = "unknown"))))]
+#[cfg(all(
+    feature = "libc",
+    not(all(target_family = "wasm", target_os = "unknown"))
+))]
 pub type z_off_t = libc::off_t;
 
 #[cfg(all(feature = "libc", all(target_family = "wasm", target_os = "unknown")))]
@@ -73,95 +76,115 @@ extern "C" {
     pub fn deflateBound(strm: z_streamp, sourceLen: uLong) -> uLong;
     pub fn deflateCopy(dest: z_streamp, source: z_streamp) -> c_int;
     pub fn deflateEnd(strm: z_streamp) -> c_int;
-    pub fn deflateInit_(strm: z_streamp, level: c_int,
-                        version: *const c_char,
-                        stream_size: c_int) -> c_int;
-    pub fn deflateInit2_(strm: z_streamp,
-                         level: c_int,
-                         method: c_int,
-                         windowBits: c_int,
-                         memLevel: c_int,
-                         strategy: c_int,
-                         version: *const c_char,
-                         stream_size: c_int) -> c_int;
-    pub fn deflateParams(strm: z_streamp,
-                         level: c_int,
-                         strategy: c_int) -> c_int;
+    pub fn deflateInit_(
+        strm: z_streamp,
+        level: c_int,
+        version: *const c_char,
+        stream_size: c_int,
+    ) -> c_int;
+    pub fn deflateInit2_(
+        strm: z_streamp,
+        level: c_int,
+        method: c_int,
+        windowBits: c_int,
+        memLevel: c_int,
+        strategy: c_int,
+        version: *const c_char,
+        stream_size: c_int,
+    ) -> c_int;
+    pub fn deflateParams(strm: z_streamp, level: c_int, strategy: c_int) -> c_int;
     pub fn deflatePrime(strm: z_streamp, bits: c_int, value: c_int) -> c_int;
     pub fn deflateReset(strm: z_streamp) -> c_int;
-    pub fn deflateSetDictionary(strm: z_streamp,
-                                dictionary: *const Bytef,
-                                dictLength: uInt) -> c_int;
+    pub fn deflateSetDictionary(
+        strm: z_streamp,
+        dictionary: *const Bytef,
+        dictLength: uInt,
+    ) -> c_int;
     pub fn deflateSetHeader(strm: z_streamp, head: gz_headerp) -> c_int;
-    pub fn deflateTune(strm: z_streamp,
-                       good_length: c_int,
-                       max_lazy: c_int,
-                       nice_length: c_int,
-                       max_chain: c_int) -> c_int;
+    pub fn deflateTune(
+        strm: z_streamp,
+        good_length: c_int,
+        max_lazy: c_int,
+        nice_length: c_int,
+        max_chain: c_int,
+    ) -> c_int;
     pub fn inflate(strm: z_streamp, flush: c_int) -> c_int;
-    pub fn inflateBack(strm: z_streamp,
-                       _in: in_func,
-                       in_desc: *mut c_void,
-                       out: out_func,
-                       out_desc: *mut c_void) -> c_int;
+    pub fn inflateBack(
+        strm: z_streamp,
+        _in: in_func,
+        in_desc: *mut c_void,
+        out: out_func,
+        out_desc: *mut c_void,
+    ) -> c_int;
     pub fn inflateBackEnd(strm: z_streamp) -> c_int;
-    pub fn inflateBackInit_(strm: z_streamp,
-                            windowBits: c_int,
-                            window: *mut c_uchar,
-                            version: *const c_char,
-                            stream_size: c_int) -> c_int;
+    pub fn inflateBackInit_(
+        strm: z_streamp,
+        windowBits: c_int,
+        window: *mut c_uchar,
+        version: *const c_char,
+        stream_size: c_int,
+    ) -> c_int;
     pub fn inflateCopy(dest: z_streamp, source: z_streamp) -> c_int;
     pub fn inflateEnd(strm: z_streamp) -> c_int;
     pub fn inflateGetHeader(strm: z_streamp, head: gz_headerp) -> c_int;
-    pub fn inflateInit_(strm: z_streamp,
-                        version: *const c_char,
-                        stream_size: c_int) -> c_int;
-    pub fn inflateInit2_(strm: z_streamp,
-                         windowBits: c_int,
-                         version: *const c_char,
-                         stream_size: c_int) -> c_int;
+    pub fn inflateInit_(strm: z_streamp, version: *const c_char, stream_size: c_int) -> c_int;
+    pub fn inflateInit2_(
+        strm: z_streamp,
+        windowBits: c_int,
+        version: *const c_char,
+        stream_size: c_int,
+    ) -> c_int;
     pub fn inflateMark(strm: z_streamp) -> c_long;
     pub fn inflatePrime(strm: z_streamp, bits: c_int, value: c_int) -> c_int;
     pub fn inflateReset(strm: z_streamp) -> c_int;
     pub fn inflateReset2(strm: z_streamp, windowBits: c_int) -> c_int;
-    pub fn inflateSetDictionary(strm: z_streamp,
-                                dictionary: *const Bytef,
-                                dictLength: uInt) -> c_int;
+    pub fn inflateSetDictionary(
+        strm: z_streamp,
+        dictionary: *const Bytef,
+        dictLength: uInt,
+    ) -> c_int;
     pub fn inflateSync(strm: z_streamp) -> c_int;
     pub fn zlibCompileFlags() -> uLong;
     pub fn zlibVersion() -> *const c_char;
 
-
-// The above set of functions currently target 1.2.3.4 (what's present on Ubuntu
-// 12.04, but there's some other APIs that were added later. Should figure out
-// how to expose them...
-//
-// Added in 1.2.5.1
-//
-//     pub fn deflatePending(strm: z_streamp,
-//                           pending: *mut c_uint,
-//                           bits: *mut c_int) -> c_int;
-//
-// Addedin 1.2.7.1
-//     pub fn inflateGetDictionary(strm: z_streamp,
-//                                 dictionary: *mut Bytef,
-//                                 dictLength: *mut uInt) -> c_int;
-//
-// Added in 1.2.3.5
-//     pub fn gzbuffer(file: gzFile, size: c_uint) -> c_int;
-//     pub fn gzclose_r(file: gzFile) -> c_int;
-//     pub fn gzclose_w(file: gzFile) -> c_int;
-//     pub fn gzoffset(file: gzFile) -> z_off_t;
+    // The above set of functions currently target 1.2.3.4 (what's present on Ubuntu
+    // 12.04, but there's some other APIs that were added later. Should figure out
+    // how to expose them...
+    //
+    // Added in 1.2.5.1
+    //
+    //     pub fn deflatePending(strm: z_streamp,
+    //                           pending: *mut c_uint,
+    //                           bits: *mut c_int) -> c_int;
+    //
+    // Addedin 1.2.7.1
+    //     pub fn inflateGetDictionary(strm: z_streamp,
+    //                                 dictionary: *mut Bytef,
+    //                                 dictLength: *mut uInt) -> c_int;
+    //
+    // Added in 1.2.3.5
+    //     pub fn gzbuffer(file: gzFile, size: c_uint) -> c_int;
+    //     pub fn gzclose_r(file: gzFile) -> c_int;
+    //     pub fn gzclose_w(file: gzFile) -> c_int;
+    //     pub fn gzoffset(file: gzFile) -> z_off_t;
 }
 
 #[cfg(feature = "libc")]
 extern "C" {
     pub fn adler32_combine(adler1: uLong, adler2: uLong, len2: z_off_t) -> uLong;
-    pub fn compress(dest: *mut Bytef, destLen: *mut uLongf,
-                    source: *const Bytef, sourceLen: uLong) -> c_int;
-    pub fn compress2(dest: *mut Bytef, destLen: *mut uLongf,
-                     source: *const Bytef, sourceLen: uLong,
-                     level: c_int) -> c_int;
+    pub fn compress(
+        dest: *mut Bytef,
+        destLen: *mut uLongf,
+        source: *const Bytef,
+        sourceLen: uLong,
+    ) -> c_int;
+    pub fn compress2(
+        dest: *mut Bytef,
+        destLen: *mut uLongf,
+        source: *const Bytef,
+        sourceLen: uLong,
+        level: c_int,
+    ) -> c_int;
     pub fn compressBound(sourceLen: uLong) -> uLong;
     pub fn crc32_combine(crc1: uLong, crc2: uLong, len2: z_off_t) -> uLong;
     pub fn gzdirect(file: gzFile) -> c_int;
@@ -183,10 +206,12 @@ extern "C" {
     pub fn gztell(file: gzFile) -> z_off_t;
     pub fn gzungetc(c: c_int, file: gzFile) -> c_int;
     pub fn gzwrite(file: gzFile, buf: voidpc, len: c_uint) -> c_int;
-    pub fn uncompress(dest: *mut Bytef,
-                      destLen: *mut uLongf,
-                      source: *const Bytef,
-                      sourceLen: uLong) -> c_int;
+    pub fn uncompress(
+        dest: *mut Bytef,
+        destLen: *mut uLongf,
+        source: *const Bytef,
+        sourceLen: uLong,
+    ) -> c_int;
 }
 
 pub const Z_NO_FLUSH: c_int = 0;
